3857932076a4cb3b4a03c486a64f8894b3feb0cd
sthhhh291 <sthhhh291@gmail.com>
2025-07-02 08:01:14 -0500

    added customer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomersComponent } from "./customers/customers.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
