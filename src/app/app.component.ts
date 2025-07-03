b54c21a4816e610f61c0faebab3215246c0b7b9f
sthhhh291 <sthhhh291@gmail.com>
2025-07-03 12:16:02 -0500

    updates to customer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CustomersComponent } from "./customers/customers.component";
+import { CustomersComponent } from './customers/customers.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CustomersComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'shop1';
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Customer } from '../customer';
 
 @Component({
   selector: 'app-customers',
   imports: [],
   templateUrl: './customers.component.html',
-  styleUrl: './customers.component.css'
+  styleUrl: './customers.component.css',
 })
 export class CustomersComponent {
-
+  customers: Customer[] = [
+    {
+      id: 1,
+      first_name: 'John',
+      last_name: 'Doe',
+      notes: 'Regular customer',
+    },
+    {
+      id: 2,
+      first_name: 'Jane',
+      last_name: 'Smith',
+      notes: 'VIP customer',
+    },
+  ];
 }
