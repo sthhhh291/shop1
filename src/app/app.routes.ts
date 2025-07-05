46de74359dd9c0699fb1cd430905caf7f8cd3633
sthhhh291 <sthhhh291@gmail.com>
2025-07-05 10:04:40 -0500

    route changes. not sure if i like this

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CustomersComponent } from './customers/customers.component';
 import { AddCustomerComponent } from './customers/components/add-customer/add-customer.component';
 import { CustomerComponent } from './customer/customer.component';
+import { SearchComponent } from './customers/components/search/search.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'customers', pathMatch: 'full' },
@@ -10,6 +11,7 @@ export const routes: Routes = [
     component: CustomersComponent,
     children: [
       { path: 'add', component: AddCustomerComponent },
+      { path: 'search', component: SearchComponent },
       { path: ':id', component: CustomerComponent },
     ],
   },
---
+++ src/app/customers/components/search/search.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { CustomersService } from '../../../services/customers.service';
+import { Customer } from '../../../customer';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { AddCustomerComponent } from '../add-customer/add-customer.component';
+
+@Component({
+  selector: 'app-customers',
+  imports: [JsonPipe, AddCustomerComponent, RouterModule],
+  providers: [CustomersService],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent {
+  filter: string = '';
+  page: number = 1;
+  limit: number = 30;
+  isAddCustomer: boolean = false;
+  // private route: ActivatedRoute;
+  customers: { totals: any; customers: Customer[] } = {
+    totals: {},
+    customers: [],
+  };
+  constructor(
+    private customersService: CustomersService,
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.limit = Number(params['limit']) || 30;
+      this.page = Number(params['page']) || 1;
+      this.filter = params['filter'] || '';
+      this.customersService
+        .getCustomers(this.filter, this.page, this.limit)
+        .subscribe((customers) => {
+          console.log('customers', customers.customers);
+          this.customers = customers;
+        });
+    });
+  }
+  updateFilter(event: any) {
+    this.filter = event.target.value;
+    this.page = 1;
+    console.log('filter', this.filter);
+    this.router.navigate([], {
+      queryParams: { filter: this.filter, page: this.page },
+      queryParamsHandling: 'merge', // Preserve other query parameters
+    });
+  }
+  updatePage(event: any) {
+    this.page = event.target.value;
+    this.router.navigate([], {
+      queryParams: { page: this.page },
+      queryParamsHandling: 'merge',
+    });
+  }
+  updateLimit(event: any) {
+    this.limit = event.target.value;
+    this.page = 1; // Reset to first page when limit changes
+    this.router.navigate([], {
+      queryParams: { limit: this.limit, page: this.page },
+      queryParamsHandling: 'merge',
+    });
+  }
+}
