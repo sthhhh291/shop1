266c69c4048f5a63c565c0074c52d9789b4eb678
sthhhh291 <sthhhh291@gmail.com>
2025-07-14 18:22:52 -0500

    started work on repair page. needs styling and car,customer and phone cards

--- src/app/repair/repair.component.ts
+++ src/app/repair/repair.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, Inject, input, signal } from '@angular/core';
+import { RepairsService } from '../services/repairs.service';
+import { Estimate } from '../types/estimate';
+import { JsonPipe } from '@angular/common';
+import { Labor } from '../types/labor';
+import { Part } from '../types/part';
+import { Totals } from '../types/totals';
 @Component({
   selector: 'app-repair',
-  imports: [],
+  imports: [JsonPipe],
+  providers: [RepairsService],
   templateUrl: './repair.component.html',
-  styleUrl: './repair.component.css'
+  styleUrl: './repair.component.css',
 })
 export class RepairComponent {
-
+  repairService = inject(RepairsService);
+  repair = signal<Estimate | null>(null);
+  labor = signal<Labor[]>([]);
+  parts = signal<Part[]>([]);
+  oil = signal<Part[]>([]);
+  totals = signal<Totals | null>(null);
+  id = input.required<string>();
+  ngOnInit() {
+    this.repairService.getRepair(Number(this.id())).subscribe((data) => {
+      console.log('repair data:', data);
+      this.repair.set(data);
+    });
+    this.repairService.getRepairLabor(Number(this.id())).subscribe((data) => {
+      this.labor.set(data);
+    });
+    this.repairService.getRepairParts(Number(this.id())).subscribe((data) => {
+      this.parts.set(data);
+    });
+    this.repairService.getRepairOil(Number(this.id())).subscribe((data) => {
+      this.oil.set(data);
+    });
+    this.repairService.getRepairTotals(Number(this.id())).subscribe((data) => {
+      this.totals.set(data);
+    });
+    console.log('repair data:', this.repair());
+    console.log('labor data:', this.labor());
+    console.log('parts data:', this.parts());
+    console.log('oil data:', this.oil());
+    console.log('totals data:', this.totals());
+  }
 }
--- src/app/services/repairs.service.ts
+++ src/app/services/repairs.service.ts
@@ -17,34 +17,45 @@ export class RepairsService {
     limit: number
   ): Observable<{ repairs: Estimate[]; totals: any }> {
     return this.http.get<{ repairs: Estimate[]; totals: any }>(
-      `http://localhost:3001/repairs?filter=${filter}&page=${page}&limit=${limit}`
+      `http://localhost:3001/repair_orders?filter=${filter}&page=${page}&limit=${limit}`
     );
   }
   getRepair(id: number): Observable<Estimate> {
-    return this.http.get<Estimate>(`http://localhost:3001/repairs/${id}`);
+    return this.http.get<Estimate>(`http://localhost:3001/repair_orders/${id}`);
   }
   getRepairLabor(id: number): Observable<Labor[]> {
-    return this.http.get<Labor[]>(`http://localhost:3001/repairs/${id}/labor`);
+    return this.http.get<Labor[]>(
+      `http://localhost:3001/repair_orders/${id}/labor`
+    );
   }
   getRepairParts(id: number): Observable<Part[]> {
-    return this.http.get<Part[]>(`http://localhost:3001/repairs/${id}/parts`);
+    return this.http.get<Part[]>(
+      `http://localhost:3001/repair_orders/${id}/parts`
+    );
   }
   getRepairOil(id: number): Observable<Part[]> {
-    return this.http.get<Part[]>(`http://localhost:3001/repairs/${id}/oil`);
+    return this.http.get<Part[]>(
+      `http://localhost:3001/repair_orders/${id}/oil`
+    );
   }
   getRepairTotals(id: number): Observable<Totals> {
-    return this.http.get<Totals>(`http://localhost:3001/repairs/${id}/totals`);
+    return this.http.get<Totals>(
+      `http://localhost:3001/repair_orders/${id}/totals`
+    );
   }
   addRepair(repair: Estimate): Observable<any> {
-    return this.http.post<Estimate>('http://localhost:3001/repairs', repair);
+    return this.http.post<Estimate>(
+      'http://localhost:3001/repair_orders',
+      repair
+    );
   }
   updateRepair(repair: Estimate): Observable<Estimate> {
     return this.http.put<Estimate>(
-      `http://localhost:3001/repairs/${repair.id}`,
+      `http://localhost:3001/repair_orders/${repair.id}`,
       repair
     );
   }
   deleteRepair(id: number): Observable<void> {
-    return this.http.delete<void>(`http://localhost:3001/repairs/${id}`);
+    return this.http.delete<void>(`http://localhost:3001/repair_orders/${id}`);
   }
 }
